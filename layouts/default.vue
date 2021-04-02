e53a9dbf883f6e92e39f4ebb1076747430f148d4
tugskusunoki <tugs@kusunoki.com>
2021-04-02 15:55:00 +0900

    1

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Nuxt/>
+  </div>
+</template>
+
+<style>
+
+
+html,
+body {
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
+  background-color: black;
+  font-size: $fontSize14;
+}
+
+.container {
+  padding: 0 27px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <h2>Text</h2>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id libero vel lacus ultrices congue id a tellus.
+    Suspendisse nec libero aliquam, lobortis est ac, placerat turpis. Aliquam non nunc nec massa convallis cursus vel eu
+    dui. Nullam turpis ipsum, tincidunt non feugiat vel, ultricies eget nibh. Nullam semper arcu vitae nisl lacinia
+    lobortis non ut quam. Integer rutrum euismod mauris non hendrerit. Donec nisi velit, iaculis a rutrum eu, finibus eu
+    orci. Curabitur quis lacus ex. Aenean eu maximus massa. Mauris eget turpis tincidunt enim volutpat iaculis et eget
+    enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus placerat
+    aliquam ornare. Morbi eget nisl aliquam, convallis diam vehicula, suscipit nulla.
+
+    Donec ultricies felis eu dui volutpat, in dapibus quam ullamcorper. Sed ut tortor vel nisl pretium tincidunt. Sed
+    iaculis leo arcu, eu consectetur odio accumsan quis. Cras vitae finibus erat. Vivamus ut tellus felis. Donec et
+    sapien massa. Quisque luctus eros leo, in hendrerit nunc rutrum vel. Ut eu justo quis justo tincidunt hendrerit.
+    Integer tempus est ac tortor egestas, ac placerat odio tincidunt. Integer nibh quam, hendrerit sed imperdiet sed,
+    scelerisque sit amet libero. Suspendisse lacinia, orci pretium semper sollicitudin, lectus tellus mattis ex, eget
+    semper velit neque id leo. Mauris efficitur ligula non erat posuere cursus. Quisque consequat est condimentum sapien
+    eleifend mattis. Fusce pulvinar eros est.
+
+    Nullam consectetur, nibh a ultricies aliquam, elit purus lacinia lorem, vitae gravida lacus ante nec massa. Class
+    aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Class aptent taciti sociosqu
+    ad litora torquent per conubia nostra, per inceptos himenaeos. Cras tortor nisl, feugiat vehicula vulputate non,
+    sodales nec elit. Donec bibendum velit nibh, et malesuada ante molestie et. Curabitur auctor enim quis nisl sodales
+    hendrerit. Integer malesuada lectus ac sagittis interdum. Aenean augue ante, tincidunt non fringilla in, elementum
+    eget sapien. Aliquam sit amet commodo tortor. In maximus imperdiet ex sed scelerisque. Praesent elit odio, tempus
+    sit amet fermentum sit amet, rutrum sed elit. Nulla scelerisque, libero auctor consequat mattis, erat leo porttitor
+    ligula, sit amet sodales justo lacus at velit. Pellentesque habitant morbi tristique senectus et netus et malesuada
+    fames ac turpis egestas.
+
+    Nulla condimentum venenatis est eget pellentesque. Integer in purus scelerisque, malesuada nisl a, ultricies orci.
+    Nulla cursus ultrices neque id posuere. Ut pretium ipsum a est egestas faucibus. In urna justo, congue posuere
+    mollis vulputate, luctus quis purus. Nunc metus diam, porta a dapibus id, maximus ac ante. Vestibulum sed tristique
+    leo. Curabitur dui nunc, lobortis nec sodales ut, iaculis vel dolor. Ut euismod malesuada accumsan. Quisque aliquet
+    arcu euismod porttitor laoreet. Fusce scelerisque massa lorem. Donec non nisl tempor tortor fermentum sodales vel
+    nec massa.
+
+    Donec dignissim facilisis augue eget ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Suspendisse efficitur neque nibh, ac mollis mauris tincidunt at. Aenean ac nulla neque. Proin condimentum semper
+    tristique. Aliquam non nunc eget libero pellentesque dictum. In hac habitasse platea dictumst. Nulla scelerisque
+    sapien et turpis tincidunt, at iaculis libero consectetur. Cras condimentum tempor metus, vitae vehicula felis
+    tincidunt sit amet. Morbi pretium quam eu libero viverra, vitae viverra lorem auctor. Mauris vehicula diam eget enim
+    ullamcorper malesuada.
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+.container {
+  max-width: 750px;
+  margin: 0 auto;
+  color: white;
+  background-color: grey;
+}
+</style>
